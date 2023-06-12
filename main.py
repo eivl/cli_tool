9e4dfe4fe5593622de72809ad51bcdae79e25d0d
Eivind Teig <eivind.teig@gmail.com>
2023-06-12 15:50:01 +0200

    Add movie rating command.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 import typer
 
+from movie import movie_search
 
 app = typer.Typer()
 
@@ -19,6 +20,12 @@ def hello(name: Optional[str] = None):
 def bye():
     typer.echo("Bye bye!")
 
+
+@app.command()
+def movie_rating(movie_name: str):
+    """Get the rating for a movie."""
+    movie = movie_search(movie_name)
+    typer.echo(f"Rating for {movie_name.title()} {movie['vote_average']}")
 
 
 if __name__ == "__main__":
